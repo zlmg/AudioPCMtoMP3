f86dfe87d423554cc71c12f90b4d1f46100afecd
zlmg <zlmg4@163.com>
2016-03-22 19:01:15 +0800

    init commit

--- AudioPCMtoMP3/ViewController.m
+++ AudioPCMtoMP3/ViewController.m
@@ -7,21 +7,72 @@
 //
 
 #import "ViewController.h"
+#import "lame.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIButton *recordButton;
+@property (nonatomic, strong) UIButton *playButton;
+@property (nonatomic, strong) UIButton *resetButton;
+@property (nonatomic, strong) UIButton *doneButton;
 
 @end
 
 @implementation ViewController
-
+#pragma mark - init
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.recordButton];
+    [self.view addSubview:self.playButton];
+    [self.view addSubview:self.resetButton];
+    [self.view addSubview:self.doneButton];
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self initRecorder];
+    });
+}
+
+- (void)initRecorder {
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma  mark - getter
+- (UIButton *)recordButton {
+    if (nil == _recordButton) {
+        _recordButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_recordButton setTitle:@"record" forState:UIControlStateNormal];
+        [_recordButton setTitle:@"stop" forState:UIControlStateSelected];
+        
+    }
+    return _recordButton;
+}
+
+- (UIButton *)playButton {
+    if (nil == _playButton) {
+        _playButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_playButton setTitle:@"play" forState:UIControlStateNormal];
+    }
+    return  _playButton;
+}
+
+- (UIButton *)resetButton {
+    if (nil == _resetButton) {
+        _resetButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_resetButton setTitle:@"reset" forState:UIControlStateNormal];
+    }
+    return _resetButton;
+}
+
+- (UIButton *)doneButton {
+    if (nil == _doneButton) {
+        _doneButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [_doneButton setTitle:@"done" forState:UIControlStateNormal];
+    }
+    return  _doneButton;
 }
 
 @end
