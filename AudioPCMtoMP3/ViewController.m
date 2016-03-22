5c96eccf8a3ea8bf47ddfea82e781120ddd391e8
zlmg <zlmg4@163.com>
2016-03-22 17:51:26 +0800

    Initial Commit

---
+++ AudioPCMtoMP3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AudioPCMtoMP3
+//
+//  Created by Allan on 3/22/16.
+//  Copyright © 2016 zlmg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AudioPCMtoMP3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioPCMtoMP3
+//
+//  Created by Allan on 3/22/16.
+//  Copyright © 2016 zlmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
